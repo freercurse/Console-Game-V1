2f81a1fd7f379e5e3af8b46b5cba671544e767f0
freercurse <freerjoseph@yahoo.co.yk>
2022-03-20 02:16:32 +0000

    Start of gameState planning

---
+++ Console Game/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Game
+{
+  class GameState
+  {
+    public int time;
+    public double DailyLuck;
+    public int week;
+    public double WeeklyInterest;
+    public int WinningBetlimit;
+  }
+}
